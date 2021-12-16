edd284a88e7e750d2b1141a99102f7f97658a9f2
Antonin Folliasson <antonin@aitio.co>
2021-12-16 20:04:22 +0100

    ok pour le premier à deux tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+(1..5).each do |my_retaurant|
+  my_restaurant = Restaurant.new(name: Faker::Restaurant.name, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone)
+  my_restaurant.save
+  the_review = Review.new(content: Faker::Restaurant.review, rating: (0..5).to_a.sample)
+  the_review.restaurant = my_restaurant
+  the_review.save
+end
